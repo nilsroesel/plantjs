0622cf20118176e6ad1c882adc905e9ab04e18eb
nilsroesel <nils.roesel@yahoo.de>
2018-05-26 21:53:40 +0200

    Create http request wrapper class

---
+++ src/util/wrapper/request.wrapper.ts
@@ -0,0 +1,41 @@
+import { IncomingHttpHeaders, IncomingMessage } from 'http';
+
+export class Request {
+    static readRequestBody(request: IncomingMessage): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            let body = [];
+            request.on('error', (err) => {
+                reject(err);
+            }).on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                resolve(Buffer.concat(body).toString());
+            });
+        });
+
+    }
+
+    readonly url: string;
+    readonly params: Object;
+    readonly headers: IncomingHttpHeaders;
+    readonly method: string;
+    readonly body: string;
+
+    constructor(request: IncomingMessage, body: string, params: Object) {
+        this.url = request.url;
+        this.params = params;
+        this.headers = request.headers;
+        this.method = request.method;
+        this.body = body;
+    }
+
+    json(): Object {
+        try {
+            return JSON.parse(this.body);
+        } catch (e) {
+            return {};
+        }
+    }
+
+
+}
