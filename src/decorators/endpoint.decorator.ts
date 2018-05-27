058b09ca9ffec53f74542f39c9a788605ab1cbf7
nilsroesel <nils.roesel@yahoo.de>
2018-05-27 16:34:52 +0200

    Check with @Endpoint annotated properties index signatures to fit the model and throw error on not fit

--- src/decorators/endpoint.decorator.ts
+++ src/decorators/endpoint.decorator.ts
@@ -1,8 +1,17 @@
-import { HandlerFn } from '../index';
+import 'reflect-metadata';
+import { Request, Response } from '../index';
 
 export function Endpoint(route?: string) {
+    function checkEndpointIndexSignature(target: Object, key: string) {
+        const types = Reflect.getMetadata('design:paramtypes', target, key) || [null, null];
+        if (types[0] !== Request || types[1] !== Response || types.length !== 2) {
+            const errorMessage = `Index signature of ${key}(${types.map(a => a.name).join()}) does not fit (req Request, res Response) => void`;
+            throw new Error(errorMessage);
+        }
+    }
     return (target: Object, key: string) => {
-        if (target['skeidjs'] && target['skeidjs'] instanceof Array && target[key] === Function) {
+        checkEndpointIndexSignature(target, key);
+        if (target['skeidjs'] && target['skeidjs'] instanceof Array) {
             (target['skeidjs'] as Array<EndpointHandler>).push({
                 functionInstance: target,
                 functionKey: key,
