23bbdd3fcce9924a09d8550a70aef4f55619a0e3
nilsroesel <nils.roesel@yahoo.de>
2018-05-29 20:32:32 +0200

    Add middleware iterator to index

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,4 +1,5 @@
 export * from './wrapper/index';
 export * from './route';
 export * from './router';
-export * from './injector'
+export * from './injector';
+export * from './middleware.iterator';
