bbd8f25844d8de5d9ca4f30e9fcf505df12c17bd
nilsroesel <nils.roesel@yahoo.de>
2018-06-05 19:50:36 +0200

    Auto generated documentation

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -3,6 +3,9 @@ typedoc.search = typedoc.search || {};
 typedoc.search.data = {
     "kinds": {
         "1": "External module",
+        "4": "Enumeration",
+        "16": "Enumeration member",
+        "32": "Variable",
         "64": "Function",
         "128": "Class",
         "256": "Interface",
@@ -21,51 +24,51 @@ typedoc.search.data = {
     }, {
         "id": 1,
         "kind": 64,
-        "name": "Component",
-        "url": "modules/decorators.html#component",
-        "classes": "tsd-kind-function tsd-parent-kind-external-module",
-        "parent": "Decorators"
-    }, {
-        "id": 2,
-        "kind": 64,
         "name": "Endpoint",
         "url": "modules/decorators.html#endpoint",
         "classes": "tsd-kind-function tsd-parent-kind-external-module",
         "parent": "Decorators"
     }, {
-        "id": 3,
+        "id": 2,
         "kind": 1,
         "name": "Types",
         "url": "modules/types.html",
         "classes": "tsd-kind-external-module"
     }, {
+        "id": 3,
+        "kind": 256,
+        "name": "ContextHandlerFn",
+        "url": "interfaces/types.contexthandlerfn.html",
+        "classes": "tsd-kind-interface tsd-parent-kind-external-module",
+        "parent": "Types"
+    }, {
         "id": 4,
+        "kind": 1024,
+        "name": "functionContextInstance",
+        "url": "interfaces/types.contexthandlerfn.html#functioncontextinstance",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "Types.ContextHandlerFn"
+    }, {
+        "id": 5,
+        "kind": 1024,
+        "name": "fn",
+        "url": "interfaces/types.contexthandlerfn.html#fn",
+        "classes": "tsd-kind-property tsd-parent-kind-interface",
+        "parent": "Types.ContextHandlerFn"
+    }, {
+        "id": 6,
         "kind": 4194304,
         "name": "HandlerFn",
         "url": "modules/types.html#handlerfn",
         "classes": "tsd-kind-type-alias tsd-parent-kind-external-module",
         "parent": "Types"
     }, {
-        "id": 5,
-        "kind": 65536,
-        "name": "__type",
-        "url": "modules/types.html#handlerfn.__type-1",
-        "classes": "tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported",
-        "parent": "Types.HandlerFn"
-    }, {
-        "id": 6,
-        "kind": 4194304,
-        "name": "ContextHandlerFn",
-        "url": "modules/types.html#contexthandlerfn",
-        "classes": "tsd-kind-type-alias tsd-parent-kind-external-module",
-        "parent": "Types"
-    }, {
         "id": 7,
         "kind": 65536,
         "name": "__type",
-        "url": "modules/types.html#contexthandlerfn.__type",
+        "url": "modules/types.html#handlerfn.__type",
         "classes": "tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported",
-        "parent": "Types.ContextHandlerFn"
+        "parent": "Types.HandlerFn"
     }, {
         "id": 8,
         "kind": 4194304,
@@ -88,235 +91,270 @@ typedoc.search.data = {
         "classes": "tsd-kind-external-module"
     }, {
         "id": 11,
-        "kind": 64,
-        "name": "Injectable",
-        "url": "modules/decorators.html#injectable",
-        "classes": "tsd-kind-function tsd-parent-kind-external-module",
-        "parent": "Decorators"
-    }, {
-        "id": 12,
         "kind": 1,
         "name": "Decorator-Configs",
         "url": "modules/decorator_configs.html",
         "classes": "tsd-kind-external-module"
     }, {
-        "id": 13,
+        "id": 12,
         "kind": 256,
         "name": "ApplicationConfig",
         "url": "interfaces/decorator_configs.applicationconfig.html",
         "classes": "tsd-kind-interface tsd-parent-kind-external-module",
         "parent": "Decorator-Configs"
     }, {
-        "id": 14,
+        "id": 13,
         "kind": 1024,
         "name": "contentType",
         "url": "interfaces/decorator_configs.applicationconfig.html#contenttype",
         "classes": "tsd-kind-property tsd-parent-kind-interface",
         "parent": "Decorator-Configs.ApplicationConfig"
     }, {
-        "id": 15,
+        "id": 14,
         "kind": 1024,
         "name": "server",
         "url": "interfaces/decorator_configs.applicationconfig.html#server",
         "classes": "tsd-kind-property tsd-parent-kind-interface",
         "parent": "Decorator-Configs.ApplicationConfig"
     }, {
-        "id": 16,
+        "id": 15,
         "kind": 65536,
         "name": "__type",
         "url": "interfaces/decorator_configs.applicationconfig.html#server.__type",
         "classes": "tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported",
         "parent": "Decorator-Configs.ApplicationConfig.server"
     }, {
-        "id": 17,
+        "id": 16,
         "kind": 1024,
         "name": "middleware",
         "url": "interfaces/decorator_configs.applicationconfig.html#middleware",
         "classes": "tsd-kind-property tsd-parent-kind-interface",
         "parent": "Decorator-Configs.ApplicationConfig"
     }, {
-        "id": 18,
+        "id": 17,
         "kind": 1024,
         "name": "components",
         "url": "interfaces/decorator_configs.applicationconfig.html#components",
         "classes": "tsd-kind-property tsd-parent-kind-interface",
         "parent": "Decorator-Configs.ApplicationConfig"
     }, {
-        "id": 19,
+        "id": 18,
         "kind": 256,
         "name": "ComponentConfig",
         "url": "interfaces/decorator_configs.componentconfig.html",
         "classes": "tsd-kind-interface tsd-parent-kind-external-module",
         "parent": "Decorator-Configs"
     }, {
-        "id": 20,
+        "id": 19,
         "kind": 1024,
         "name": "route",
         "url": "interfaces/decorator_configs.componentconfig.html#route",
         "classes": "tsd-kind-property tsd-parent-kind-interface",
         "parent": "Decorator-Configs.ComponentConfig"
     }, {
-        "id": 21,
+        "id": 20,
         "kind": 1024,
         "name": "middleware",
         "url": "interfaces/decorator_configs.componentconfig.html#middleware",
         "classes": "tsd-kind-property tsd-parent-kind-interface",
         "parent": "Decorator-Configs.ComponentConfig"
     }, {
-        "id": 22,
+        "id": 21,
         "kind": 256,
         "name": "EndpointConfig",
         "url": "interfaces/decorator_configs.endpointconfig.html",
         "classes": "tsd-kind-interface tsd-parent-kind-external-module",
         "parent": "Decorator-Configs"
     }, {
-        "id": 23,
+        "id": 22,
         "kind": 1024,
         "name": "route",
         "url": "interfaces/decorator_configs.endpointconfig.html#route",
         "classes": "tsd-kind-property tsd-parent-kind-interface",
         "parent": "Decorator-Configs.EndpointConfig"
     }, {
-        "id": 24,
+        "id": 23,
         "kind": 1024,
         "name": "middleware",
         "url": "interfaces/decorator_configs.endpointconfig.html#middleware",
         "classes": "tsd-kind-property tsd-parent-kind-interface",
         "parent": "Decorator-Configs.EndpointConfig"
     }, {
+        "id": 24,
+        "kind": 32,
+        "name": "injectorStore",
+        "url": "modules/non_exported.html#injectorstore",
+        "classes": "tsd-kind-variable tsd-parent-kind-external-module",
+        "parent": "Non-exported"
+    }, {
         "id": 25,
+        "kind": 64,
+        "name": "Component",
+        "url": "modules/decorators.html#component",
+        "classes": "tsd-kind-function tsd-parent-kind-external-module",
+        "parent": "Decorators"
+    }, {
+        "id": 26,
+        "kind": 4,
+        "name": "InjectionModes",
+        "url": "enums/decorators.injectionmodes.html",
+        "classes": "tsd-kind-enum tsd-parent-kind-external-module",
+        "parent": "Decorators"
+    }, {
+        "id": 27,
+        "kind": 16,
+        "name": "SINGLE_INSTANCE",
+        "url": "enums/decorators.injectionmodes.html#single_instance",
+        "classes": "tsd-kind-enum-member tsd-parent-kind-enum",
+        "parent": "Decorators.InjectionModes"
+    }, {
+        "id": 28,
+        "kind": 16,
+        "name": "MULTI_INSTANCE",
+        "url": "enums/decorators.injectionmodes.html#multi_instance",
+        "classes": "tsd-kind-enum-member tsd-parent-kind-enum",
+        "parent": "Decorators.InjectionModes"
+    }, {
+        "id": 29,
+        "kind": 64,
+        "name": "Injectable",
+        "url": "modules/decorators.html#injectable",
+        "classes": "tsd-kind-function tsd-parent-kind-external-module",
+        "parent": "Decorators"
+    }, {
+        "id": 30,
         "kind": 1,
         "name": "Http",
         "url": "modules/http.html",
         "classes": "tsd-kind-external-module"
     }, {
-        "id": 26,
+        "id": 31,
         "kind": 128,
         "name": "Request",
         "url": "classes/http.request.html",
         "classes": "tsd-kind-class tsd-parent-kind-external-module",
         "parent": "Http"
     }, {
-        "id": 27,
+        "id": 32,
         "kind": 2048,
         "name": "readRequestBody",
         "url": "classes/http.request.html#readrequestbody",
         "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-static",
         "parent": "Http.Request"
     }, {
-        "id": 28,
+        "id": 33,
         "kind": 1024,
         "name": "url",
         "url": "classes/http.request.html#url",
         "classes": "tsd-kind-property tsd-parent-kind-class",
         "parent": "Http.Request"
     }, {
-        "id": 29,
+        "id": 34,
         "kind": 1024,
         "name": "params",
         "url": "classes/http.request.html#params",
         "classes": "tsd-kind-property tsd-parent-kind-class",
         "parent": "Http.Request"
     }, {
-        "id": 30,
+        "id": 35,
         "kind": 1024,
         "name": "headers",
         "url": "classes/http.request.html#headers",
         "classes": "tsd-kind-property tsd-parent-kind-class",
         "parent": "Http.Request"
     }, {
-        "id": 31,
+        "id": 36,
         "kind": 1024,
         "name": "method",
         "url": "classes/http.request.html#method",
         "classes": "tsd-kind-property tsd-parent-kind-class",
         "parent": "Http.Request"
     }, {
-        "id": 32,
+        "id": 37,
         "kind": 1024,
         "name": "body",
         "url": "classes/http.request.html#body",
         "classes": "tsd-kind-property tsd-parent-kind-class",
         "parent": "Http.Request"
     }, {
-        "id": 33,
+        "id": 38,
         "kind": 512,
         "name": "constructor",
         "url": "classes/http.request.html#constructor",
         "classes": "tsd-kind-constructor tsd-parent-kind-class",
         "parent": "Http.Request"
     }, {
-        "id": 34,
+        "id": 39,
         "kind": 2048,
         "name": "json",
         "url": "classes/http.request.html#json",
         "classes": "tsd-kind-method tsd-parent-kind-class",
         "parent": "Http.Request"
     }, {
-        "id": 35,
+        "id": 40,
         "kind": 128,
         "name": "Response",
         "url": "classes/http.response.html",
         "classes": "tsd-kind-class tsd-parent-kind-external-module",
         "parent": "Http"
     }, {
-        "id": 36,
+        "id": 41,
         "kind": 1024,
         "name": "response",
         "url": "classes/http.response.html#response",
         "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
         "parent": "Http.Response"
     }, {
-        "id": 37,
+        "id": 42,
         "kind": 1024,
         "name": "statusCode",
         "url": "classes/http.response.html#statuscode",
         "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
         "parent": "Http.Response"
     }, {
-        "id": 38,
+        "id": 43,
         "kind": 512,
         "name": "constructor",
         "url": "classes/http.response.html#constructor",
         "classes": "tsd-kind-constructor tsd-parent-kind-class",
         "parent": "Http.Response"
     }, {
-        "id": 39,
+        "id": 44,
         "kind": 2048,
         "name": "setHeader",
         "url": "classes/http.response.html#setheader",
         "classes": "tsd-kind-method tsd-parent-kind-class",
         "parent": "Http.Response"
     }, {
-        "id": 40,
+        "id": 45,
         "kind": 2048,
         "name": "status",
         "url": "classes/http.response.html#status",
         "classes": "tsd-kind-method tsd-parent-kind-class",
         "parent": "Http.Response"
     }, {
-        "id": 41,
+        "id": 46,
         "kind": 2048,
         "name": "text",
         "url": "classes/http.response.html#text",
         "classes": "tsd-kind-method tsd-parent-kind-class",
         "parent": "Http.Response"
     }, {
-        "id": 42,
+        "id": 47,
         "kind": 2048,
         "name": "json",
         "url": "classes/http.response.html#json",
         "classes": "tsd-kind-method tsd-parent-kind-class",
         "parent": "Http.Response"
     }, {
-        "id": 43,
+        "id": 48,
         "kind": 2048,
         "name": "send",
         "url": "classes/http.response.html#send",
         "classes": "tsd-kind-method tsd-parent-kind-class",
         "parent": "Http.Response"
     }, {
-        "id": 44,
+        "id": 49,
         "kind": 64,
         "name": "Application",
         "url": "modules/decorators.html#application",
