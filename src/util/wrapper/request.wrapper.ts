645379390a46f8c44d504ed2b022fc98d88d6005
nilsroesel <nils.roesel@gmail.com>
2018-08-19 13:58:24 +0200

    Add support for file uploading and saving

--- src/util/wrapper/request.wrapper.ts
+++ src/util/wrapper/request.wrapper.ts
@@ -3,24 +3,29 @@
  */
 import { IncomingHttpHeaders, IncomingMessage } from 'http';
 
+const fs = require('fs');
+
 /**
  * A simple facade for the node http IncomingMessage
  */
 export class Request {
-    static readRequestBody(request: IncomingMessage): Promise<string> {
-        return new Promise<string>((resolve, reject) => {
+    static readRequestBody(request: IncomingMessage): Promise<{ body: string, binary: any }> {
+        return new Promise<{ body: string, binary: any }>((resolve, reject) => {
             let body = [];
             request.on('error', (err) => {
                 reject(err);
             }).on('data', (chunk) => {
                 body.push(chunk);
             }).on('end', () => {
-                resolve(Buffer.concat(body).toString());
+                resolve({body: Buffer.concat(body).toString(), binary: Buffer.concat(body)});
+            }).on('close', () => {
+                resolve({body: Buffer.concat(body).toString(), binary: Buffer.concat(body)});
             });
         });
 
     }
 
+    private request: IncomingMessage;
     /**
      * The request url
      */
@@ -43,12 +48,16 @@ export class Request {
      */
     readonly body: string;
 
-    constructor(request: IncomingMessage, body: string, params: Object) {
+    private readonly binary;
+
+    constructor(request: IncomingMessage, body: string, binary: any, params: Object) {
+        this.request = request;
         this.url = request.url;
         this.params = params;
         this.headers = request.headers;
         this.method = request.method;
         this.body = body;
+        this.binary = binary;
     }
 
     /**
@@ -60,6 +69,15 @@ export class Request {
         } catch (e) {
             return {};
         }
+    }
+
+    /**
+     * Saves data to the given path
+     * @param targetPath Path where the file should be saved
+     */
+    file(targetPath: string) {
+        const file = fs.createWriteStream(targetPath);
+        file.write(this.binary);
     }
 
 }
