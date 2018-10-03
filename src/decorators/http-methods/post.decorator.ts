bb5de44c419290a384a0094592f70959388aae08
nilsroesel <nils.roesel@gmail.com>
2018-10-03 10:36:22 +0200

    Provide targeted version in doc comments for http method endpoint decorators

--- src/decorators/http-methods/post.decorator.ts
+++ src/decorators/http-methods/post.decorator.ts
@@ -6,6 +6,7 @@ import { EndpointFactory, EndpointTypes } from '../../internal.index';
 
 
 /**
+ * Since 1.7
  * Property Decorator Function
  * Works basically like the @Endpoint decorator, but only POST requests will be mapped to it. this will
  * enable duplicate routes on different method types
