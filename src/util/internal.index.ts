09ea29071afb0d0f8b31b024fdb118e1620c37d3
nilsroesel <nils.roesel@gmail.com>
2018-10-05 00:01:36 +0200

    Move factory classes to separate util directory

--- src/util/internal.index.ts
+++ src/util/internal.index.ts
@@ -5,5 +5,4 @@ export * from './route';
 export * from './router';
 export * from './injector';
 export * from './middleware.iterator';
-export * from './request-listener.factory';
-export * from './endpoint.factory';
+export * from './factory/internal.index';
